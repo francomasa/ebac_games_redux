2d5cca7f0f75fb3e9816e9cd020837a1644351e6
Franco <francoezequielmassaccesi@gmail.com>
2025-09-05 19:40:53 -0300

    configurando tests com redux

---
+++ src/components/Header/__tests__/index.ts
@@ -0,0 +1,43 @@
+import { screen } from '@testing-library/react';
+
+import Header from '../index';
+import { renderizaComProvider } from '../../../utils/tests';
+
+
+describe(`Teste para o componente header`, () => {
+  test(`Deve renderizar corretamente`, () => {
+    renderizaComProvider(<Header />);
+    expect(screen.getByText('EBAC Games')).toBeInTheDocument()
+  })
+
+  test(`Deve renderizar com 2 itens no carrinho`, () => {
+    renderizaComProvider(<Header />, {
+      preloadedState: {
+        carrinho: {
+          itens: {
+            {
+              id: 1,
+              categoria: 'RPG',
+              imagem: '',
+              plataformas: ['Windows', 'PS5', 'XBOX Series S/X'],
+              preco: 150.90,
+              precoAntigo: 199,90,
+              titulo: "Elden Ring"
+            },
+            {
+              id: 2,
+              categoria: 'RPG',
+              imagem: '',
+              plataformas: ['Windows'],
+              preco: 199.90,
+              precoAntigo: 299,90,
+              titulo: "Hogwarts Legacy"
+            }
+          }
+        }
+      }
+    })
+
+    expect(screen.getByTestId('qtd-carrinho').innerHTML).toContain('2 itens')
+  })
+})
---
+++ src/store/index.ts
@@ -0,0 +1,27 @@
+import {
+  configureStore,
+  combineReducers,
+  PreloadedState
+} from '@reduxjs/toolkit'
+
+import carrinhoReducer from './reducers/carrinho'
+
+import api from '../services/api'
+
+const rootReducer = combineReducers({
+  carrinho: carrinhoReducer,
+  [api.reducerPath]: api.reducer
+})
+
+export function configureStore(preloadedState?: PreloadedState<RootState>) {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware: () => any[]) =>
+      getDefaultMiddleware().concat(api.middleware),
+    preloadedState
+  })
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+
+export type AppStore = ReturnType<typeof configureStore>
