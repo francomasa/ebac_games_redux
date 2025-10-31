efbe5c8fb2af24c17c1489ea992ef0e85bc6d9a2
Franco <francoezequielmassaccesi@gmail.com>
2025-10-31 20:02:27 -0300

    Implementação de testes completa

--- src/containers/Produtos/__tests__/Produtos.test.tsx
+++ src/containers/Produtos/__tests__/Produtos.test.tsx
@@ -1,6 +1,6 @@
-import { http, HttpResponse } from 'msw'
+import { rest } from 'msw'
 import { setupServer } from 'msw/node'
-import { screen, waitFor } from '@testing-library/dom'
+import { screen, waitFor } from '@testing-library/react'
 
 import Produtos from '..'
 import { renderizaComProvider } from '../../../utils/tests'
@@ -37,7 +37,7 @@ const mocks = [
     id: 4,
     categoria: 'Aventura',
     imagem: '',
-    plataformas: ['Nintento Switch'],
+    plataformas: ['Nintendo Switch'],
     preco: 189.9,
     precoAntigo: 299.9,
     titulo: 'Donkey Kong'
@@ -45,29 +45,105 @@ const mocks = [
 ]
 
 const server = setupServer(
-  http.get('http://localhost4000/produtos', () => {
-    // requisisao: todo o que envio ao servedor
-    // resposta: e a resposta que vamos devolver
-    // contexto: a traves dele que vamos a construir a resposta
-    return HttpResponse.json(mocks)
-  })
+  rest.get(
+    'http://localhost:4000/produtos',
+    (requisicao, resposta, contexto) => {
+      return resposta(contexto.json(mocks))
+    }
+  )
 )
 
-describe('Teste para o container orodutos', () => {
+describe('Testes para o container produtos', () => {
   beforeAll(() => server.listen())
   afterEach(() => server.resetHandlers())
   afterAll(() => server.close())
 
-  test('Deve renderizar corretamente com o texto de Carregando', () => {
+  test('Deve renderizar corretamente com o texto de carregando', () => {
     renderizaComProvider(<Produtos />)
     expect(screen.getByText('Carregando...')).toBeInTheDocument()
   })
 
   test('Deve renderizar corretamente com a listagem de jogos', async () => {
-    const { debug } = renderizaComProvider(<Produtos />)
+    renderizaComProvider(<Produtos />)
     await waitFor(() => {
-      debug()
-      expect(screen.getByText('Donkey kong')).toBeInTheDocument()
+      expect(screen.getByText('Donkey Kong')).toBeInTheDocument()
     })
   })
 })
+
+// import { rest } from 'msw'
+// import { setupServer } from 'msw/node'
+// import { screen, waitFor } from '@testing-library/dom'
+
+// import Produtos from '..'
+// import { renderizaComProvider } from '../../../utils/tests'
+
+// const mocks = [
+//   {
+//     id: 1,
+//     categoria: 'RPG',
+//     imagem: '',
+//     plataformas: ['Windows'],
+//     preco: 150.9,
+//     precoAntigo: 199.9,
+//     titulo: 'Elden Ring'
+//   },
+//   {
+//     id: 2,
+//     categoria: 'RPG',
+//     imagem: '',
+//     plataformas: ['Windows', 'PS5', 'Xbox Series S/X'],
+//     preco: 199.9,
+//     precoAntigo: 299.9,
+//     titulo: 'Hogwarts Legacy'
+//   },
+//   {
+//     id: 3,
+//     categoria: 'Ação',
+//     imagem: '',
+//     plataformas: ['PS5', 'Xbox Series S/X'],
+//     preco: 150,
+//     precoAntigo: 200,
+//     titulo: 'Gotham Knights'
+//   },
+//   {
+//     id: 4,
+//     categoria: 'Aventura',
+//     imagem: '',
+//     plataformas: ['Nintento Switch'],
+//     preco: 189.9,
+//     precoAntigo: 299.9,
+//     titulo: 'Donkey Kong'
+//   }
+// ]
+
+// const server = setupServer(
+//   rest.get(
+//     'http://localhost4000/produtos',
+//     (_requisisao, resposta, contexto) => {
+//       // requisisao: todo o que envio ao servedor
+//       // resposta: e a resposta que vamos devolver
+//       // contexto: a traves dele que vamos a construir a resposta
+//       return resposta(contexto.json(mocks))
+//     }
+//   )
+// )
+
+// describe('Teste para o container orodutos', () => {
+//   beforeAll(() => server.listen())
+//   afterEach(() => server.resetHandlers())
+//   afterAll(() => server.close())
+
+//   test('Deve renderizar corretamente com o texto de Carregando', () => {
+//     renderizaComProvider(<Produtos />)
+//     expect(screen.getByText('Carregando...')).toBeInTheDocument()
+//   })
+
+//   test('Deve renderizar corretamente com a listagem de jogos', async () => {
+//     const { debug } = renderizaComProvider(<Produtos />)
+//     await waitFor(() => {
+//       debug()
+//       expect(screen.getByText('Donkey Kong')).toBeInTheDocument()
+//     })
+//   })
+// })
